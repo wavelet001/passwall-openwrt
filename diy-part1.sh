3fa276c8f7facfda826e62d4201d6ee0955d5f11
wavelet001 <58981899+wavelet001@users.noreply.github.com>
2020-12-13 15:38:03 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -6,9 +6,9 @@
 # Blog: https://p3terx.com
 #=================================================
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.100.2/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.168.1/g' package/base-files/files/bin/config_generate
 # mkdir package/base-files/files/config
 # echo 0xDEADBEEF > package/base-files/files/config/google_fu_mode
 rm -rf package/lean/smartdns
-# git clone -b lede https://github.com/pymumu/luci-app-smartdns.git feeds/luci/applications/luci-app-smartdns
-# git clone https://github.com/kenzok8/openwrt-packages.git package/openwrt-packages
+git clone -b lede https://github.com/pymumu/luci-app-smartdns.git feeds/luci/applications/luci-app-smartdns
+git clone https://github.com/kenzok8/openwrt-packages.git package/openwrt-packages
